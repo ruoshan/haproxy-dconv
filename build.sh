1b0456f464d510e2f004c2be4451fa2933428f4c
Ruoshan Huang <ruoshan.huang@gmail.com>
2016-12-01 13:49:37 +0800

    Supply version on cmd arg

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+    echo "./build.sh version"
+    exit 1
+fi
 mkdir build
-./haproxy-dconv.py -v 1.6.10 intro.txt configuration.txt management.txt -o build && cp -r css img js build
+./haproxy-dconv.py -v $1 intro.txt configuration.txt management.txt -o build && cp -r css img js build
