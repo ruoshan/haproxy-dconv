ae1940700657d55e511e93b95008a4ada635eea4
Ruoshan Huang <ruoshan.huang@gmail.com>
2018-11-21 15:51:08 +0800

    Fix css

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ if [ $# -ne 1 ]; then
     echo "./build.sh version"
     exit 1
 fi
+rm -rf build
 mkdir build
 ./dconv.py -v $1 intro.txt configuration.txt management.txt -o build && cp -r css img js build
